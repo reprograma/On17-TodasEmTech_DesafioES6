0043e1ffc06f64ca64b1b7f7be843a7e2afbcfe7
Larissa da Silva de Oliveira <larissasilvaoliveira27@gmail.com>
2022-07-17 21:19:01 -0300

    Parte 2 e CSS

--- entrega-projeto/larissa-oliveira/script.js
+++ entrega-projeto/larissa-oliveira/script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector(".input");
 const button = document.querySelector(".button-submit");
 const sectionCard = document.querySelector("#section-card");
+const sectionRepositorios = document.querySelector("#section-repositorios");
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
@@ -13,20 +14,37 @@ button.addEventListener("click", (e) => {
   input.value = " ";
 });
 
-const getGitHubData = async (user) => {
-  const url = `https://api.github.com/users/${user}`;
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    button.click();
+  }
+});
+
+const getGitHubData = async (username) => {
+  const url = `https://api.github.com/users/${username}`;
   try {
     const response = await fetch(url);
+    const responseRepos = await fetch(url + "/repos");
     if (response.ok) {
-      const data = await response.json();
+      const user = await response.json();
+      const repos = await responseRepos.json();
 
+      sectionCard.innerHTML = " ";
       const cardPrincipal = document.createElement("div");
       cardPrincipal.setAttribute("class", "card-principal");
-      cardPrincipal.innerHTML = createCard(data);
-      sectionCard.innerHTML = " ";
+      cardPrincipal.innerHTML = createCard(user);
       sectionCard.appendChild(cardPrincipal);
+
+      sectionRepositorios.innerHTML = " ";
+      repos.forEach((repo) => {
+        const repoElement = document.createElement("div");
+        repoElement.setAttribute("class", "div-repo");
+        repoElement.innerHTML = createRepo(repo);
+        sectionRepositorios.appendChild(repoElement);
+      });
     } else {
-      alert("Este usuária não existe no github");
+      alert("Usuário não encontrado");
       throw new Error();
     }
   } catch (err) {
@@ -34,15 +52,13 @@ const getGitHubData = async (user) => {
   }
 };
 
-//getGitHubData();
-
 function createCard(user) {
   const { name, avatar_url, bio, public_repos, login, followers } = user; // const name=user.name
   return `
   <h2 class= "nome" >Name: ${name}</h2>
   <img class= "img-github" src="${avatar_url}" alt="foto usuário"/>
   <h3 class= "login" >Login: ${login}</h3>
-  <p class= "bio" > Bio: ${bio ? bio : ""}</p>
+  <p class= "bio" > Bio: ${bio ?? ""}</p>
   <div class="caixa-icone-numeros">
     <img  class="icone-repositorio" src="./people_outline.jpg"/>
     <p class= "./public-repos"> ${public_repos}</p>
@@ -54,4 +70,12 @@ function createCard(user) {
   `;
 }
 
-
+function createRepo(repo) {
+  const { name, description, language, stargazers_count } = repo;
+  return `
+    <h2 class= "nome-repo"> ${name}</h2>
+    <p class= "description-repo">${description ?? "Sem descrição"}</p>
+    <p class= "language-repo">${language ?? "Sem linguagem definida"}</p>
+    <p class= "stargazers-count">⭐${stargazers_count}</p>
+    `;
+}
